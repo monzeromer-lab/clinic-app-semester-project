f483cdc87d548b0a46f786b09294fbbd7354742b
Monzer Abdullaziz <nezonru87@yahoo.com>
2021-12-14 11:29:31 +0200

    changed the import again

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// import { LitElement } from 'lit';
-import {html, render} from 'lit-html';
+import { LitElement, html } from 'lit';
+// import {html, render} from 'lit-html';
 
-// class MyElement extends LitElement {
+class MyElement extends LitElement {
 
-//     render() {
-//         return html`<h3>new</h3>`;
-//     }
-// }
-// customElements.define('my-element', MyElement);
+    render() {
+        return html`<h3>new</h3>`;
+    }
+}
+customElements.define('my-element', MyElement);
 
-const name = 'world';
-const sayHi = html`<h1>Hello ${name}</h1>`;
-render(sayHi, document.body);
+// const name = 'world';
+// const sayHi = html`<h1>Hello ${name}</h1>`;
+// render(sayHi, document.body);
