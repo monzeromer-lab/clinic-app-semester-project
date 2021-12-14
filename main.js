f9efc23368fe55869059b896eccf672915f75fd5
Monzer Abdullaziz <nezonru87@yahoo.com>
2021-12-14 09:55:40 +0200

    electron main file

---
+++ main.js
@@ -0,0 +1,25 @@
+import { app, browserWindow } from 'electron';
+
+// start new window
+function mainWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    // Load a remote URL
+    win.loadURL('https://github.com')
+
+    // Or load a local HTML file
+    // win.loadFile('index.html')
+}
+
+// When close all windows
+app.on('window-all-closed', () => {
+    app.quit()
+});
+
+// when ready
+app.on('ready', () => {
+    mainWindow();
+})
